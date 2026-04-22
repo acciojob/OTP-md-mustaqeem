96f4cc7b48f54d73a9e420d2dd77ec5221f1ff30
Md Mustaqeem <mdmustaqeem820@gmail.com>
2026-04-22 22:53:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 const codes = document.querySelectorAll(".code");
 
-// first input auto focus
 codes[0].focus();
 
 codes.forEach((code, index) => {
 
   code.addEventListener("input", (e) => {
-    if (e.target.value && index < codes.length - 1) {
+    let value = e.target.value;
+
+    // only one digit allowed
+    if (value.length > 1) {
+      value = value[0];
+      e.target.value = value;
+    }
+
+    // move forward
+    if (value !== "" && index < codes.length - 1) {
       codes[index + 1].focus();
     }
   });
 
   code.addEventListener("keydown", (e) => {
     if (e.key === "Backspace") {
-      if (code.value === "" && index > 0) {
-        codes[index - 1].value = "";     
+
+      // if current has value → clear it
+      if (code.value !== "") {
+        code.value = "";
+      } 
+      // if empty → go to previous
+      else if (index > 0) {
+        codes[index - 1].value = "";
         codes[index - 1].focus();
       }
     }
