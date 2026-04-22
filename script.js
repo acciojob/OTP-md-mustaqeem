e0eb38ba6c0a2b95fc3f33d10beb6aa65827db6d
Md Mustaqeem <mdmustaqeem820@gmail.com>
2026-04-22 22:45:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+// first input auto focus
+codes[0].focus();
+
+codes.forEach((code, index) => {
+
+  code.addEventListener("input", (e) => {
+    if (e.target.value && index < codes.length - 1) {
+      codes[index + 1].focus();
+    }
+  });
+
+  code.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      if (code.value === "" && index > 0) {
+        codes[index - 1].focus();
+      }
+    }
+  });
+
+});
